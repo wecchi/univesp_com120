3fdadeb75d5b1627e071ff65544fb56c7dbc8856
Marcelo Wecchi <wecchi@hotmail.com>
2020-10-17 19:08:04 -0300

    Objeto Point e Vector
    Melhorias no código, conforme texto base

--- Semana2/Sem2_videoaula_POO1.py
+++ Semana2/Sem2_videoaula_POO1.py
@@ -1,22 +1,19 @@
 class Point:
+    '''
+        Representação de ponto num plano cartesiano
+    '''
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
-    def set_x(self, x):
+    def setx(self, x):
         self.x = x
 
-    def set_y(self, y):
+    def sety(self, y):
         self.y = y
 
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
-
     def get(self):
-        return self.x, self.y
+        return '({0}, {1})'.format(self.x ,self.y)
 
     def move(self, offSetX, offSetY):
         self.x += offSetX
@@ -27,7 +24,12 @@ class Point:
         self.y = 0
 
     def __repr__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+        'representação de string canônica'
+        return 'Point({0}, {1})'.format(self.x ,self.y)
+
+    def __str__(self):
+        'representação de string elegante'
+        return 'Point(x = {0}, y = {1})'.format(self.x, self.y)
 
     # x + y, se (2,3) + (2,2) = (4,5)
     # x + y, se (2,3) + 8     =(10,11)
@@ -38,4 +40,62 @@ class Point:
         else:
             return Point(self.x + other,
                          self.y + other)
-        
+
+    def __eq__(self, other):
+        if type(other) == Point:
+            return ((self.x == other.x) and (self.y == other.y))
+        else:
+            return False
+
+    #8.12 Inclua o método distância() à classe Ponto.
+    # Ele apanha outro objeto Ponto como entrada e retorna a distância té esse ponto
+    # (a partir do ponto chamando o método).
+    def distancia(self,other):
+        if type(other) == Point:
+            d = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+            return d
+        else:
+            return 'Na'
+
+    #8.14 Acrescente à classe Ponto os métodos up(), down(), left() e right(), que movem o objeto Ponto em 1 unidade
+    # na direção apropriada. A implementação de cada um não deverá modificar as variáveis de instância x e y
+    # diretamente, mas indiretamente, chamando o método move() existente.
+    def up(self):
+        Point.move(self, 0, 1)
+
+    def down(self):
+        Point.move(self, 0, -1)
+
+    def left(self):
+        Point.move(self, -1, 0)
+
+    def right(self):
+        Point.move(self, 1, 0)
+
+
+##Problema Prático 8.8
+# Implemente a classe Vetor, que aceita os mesmos métodos da classe Ponto que desenvolvemos na Seção 8.4.
+# A classe Vetor também deverá aceitar a adição de vetor e operações de produto. A adição de dois vetores
+
+class Vector(Point):
+    def __repr__(self):
+        'representação de string canônica'
+        return 'Vector({0}, {1})'.format(self.x, self.y)
+
+    def __str__(self):
+        'representação de string elegante'
+        return 'Vector(x = {0}, y = {1})'.format(self.x, self.y)
+
+    def __add__(self, other):
+        if type(other) == Vector:
+            return Vector(self.x + other.x,
+                         self.y + other.y)
+        else:
+            return Vector(self.x + other,
+                         self.y + other)
+
+    def __mul__(self, other):
+        if type(other) == Vector:
+            return self.x * other.x + self.y * other.y
+        else:
+            return Vector(self.x * other, self.y * other)
